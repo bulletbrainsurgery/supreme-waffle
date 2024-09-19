41e1dcbcf098b03f6499579e6d8d4f010c5afeac
J G <neggie333@gmail.com>
2024-09-19 13:45:14 +1000

    click tracker

--- src/main/java/com/example/ExamplePlugin.java
+++ src/main/java/com/example/ExamplePlugin.java
@@ -3,9 +3,7 @@ package com.example;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
 import net.runelite.api.MenuEntry;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.MenuOptionClicked;
@@ -14,37 +12,73 @@ import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Click Tracker"
 )
-public class ExamplePlugin extends Plugin
+public class ExamplePlugin extends Plugin implements MouseListener, KeyListener
 {
-	@Inject
-	private Client client;
+	private long lastMousePress;
+	private long lastMouseRelease;
 
-	@Inject
-	private ExampleConfig config;
+	private long lastKeyPress;
+	private long lastKeyRelease;
+
+	private long startupDeltaTime;
 
 	@Override
-	protected void startUp() throws Exception
+	protected void startUp()
 	{
-		log.info("Example started!");
+		startupDeltaTime = System.currentTimeMillis();
+
+		lastMousePress = -1;
+		lastMouseRelease = -1;
+		lastKeyPress = -1;
+		lastKeyRelease = -1;
 	}
 
 	@Override
-	protected void shutDown() throws Exception
+	protected void shutDown()
 	{
-		log.info("Example stopped!");
 	}
 
 	@Subscribe
 	public void onGameStateChanged(GameStateChanged gameStateChanged)
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		lastMousePress = e.getWhen() - startupDeltaTime;
+		log.info("Mouse pressed at ("+e.getX()+", "+e.getY()+"); time " + lastMousePress + "; # clicks: " + e.getClickCount());
+
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		lastMouseRelease = e.getWhen() - startupDeltaTime;
+		log.info("Mouse released at ("+e.getX()+", "+e.getY()+"); time "+lastMouseRelease+"; # clicks: " + e.getClickCount());
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		log.info("Mouse clicked at ("+e.getX()+", "+e.getY()+"); time "+(e.getWhen() - startupDeltaTime)+"; # clicks: "+ e.getClickCount() + "; debounce "+(lastMouseRelease - lastMousePress)+"ms");
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
 	}
 
 	@Subscribe
@@ -56,5 +90,23 @@ public class ExamplePlugin extends Plugin
 	ExampleConfig provideConfig(ConfigManager configManager)
 	{
 		return configManager.getConfig(ExampleConfig.class);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		lastKeyPress = e.getWhen() - startupDeltaTime;
+		log.info("Key press: keycode "+e.getKeyCode()+ " at "+lastKeyPress);
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		lastKeyRelease = e.getWhen() - startupDeltaTime;
+		log.info("Key release: keycode "+e.getKeyCode()+ " at "+lastKeyRelease);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		log.info("Key typed: key char "+e.getKeyChar()+ " at "+(e.getWhen() - startupDeltaTime)+"; debounce "+(lastKeyRelease - lastKeyPress)+"ms");
 	}
 }
